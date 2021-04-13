5f7cf3b67881a7651e5cb008c9cbea7bacd75f3f
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-13 15:38:51 +0300

    state + observer

---
+++ Coding.Exercise.Observer/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+
+namespace Coding.Exercise.Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+
+            var rat1 = new Rat(game);
+            Console.WriteLine($"Rat1: {rat1}");
+
+            var rat2 = new Rat(game);
+            Console.WriteLine($"Rat1: {rat1}");
+            Console.WriteLine($"Rat2: {rat2}");
+
+            var rat3 = new Rat(game);
+            Console.WriteLine($"Rat1: {rat1}");
+            Console.WriteLine($"Rat2: {rat2}");
+            Console.WriteLine($"Rat3: {rat3}");
+
+            rat3.Dispose();
+            Console.WriteLine($"Rat1: {rat1}");
+            Console.WriteLine($"Rat2: {rat2}");
+            Console.WriteLine($"Rat3: {rat3}");
+        }
+    }
+
+    public class RatCountModifiedArgs : EventArgs
+    {
+        public int overallRats;
+
+        public RatCountModifiedArgs(int overallRats)
+        {
+            this.overallRats = overallRats;
+        }
+    }
+
+    public class Game
+    {
+        public event EventHandler<RatCountModifiedArgs> RatCountModified;
+
+        private int GetOverallRatCount()
+        {
+            var ratCount = RatCountModified?.GetInvocationList()?.Count();
+            return ratCount == null ? 1 : ratCount.Value;
+        }
+
+        public void HandleRatCountModification(Rat rat)
+        {
+            RatCountModified?.Invoke(rat, new RatCountModifiedArgs(GetOverallRatCount()));
+        }
+
+        // todo
+        // remember - no fields or properties!
+    }
+
+    public class Rat : IDisposable
+    {
+        private readonly Game game;
+        public int Attack = 1;
+
+        public Rat(Game game)
+        {
+            this.game = game;
+            game.RatCountModified += OnRatCountModified;
+            game.HandleRatCountModification(this);
+        }
+
+        private void OnRatCountModified(object sender, RatCountModifiedArgs e)
+        {
+            Attack = e.overallRats;
+        }
+
+        public void Dispose()
+        {
+            game.RatCountModified -= OnRatCountModified;
+            game.HandleRatCountModification(this);
+            Attack = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Attack: {Attack}";
+        }
+    }
+}
---
+++ Coding.Exercise.State/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Coding.Exercise.State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var combinationLock = new CombinationLock(new[] { 1, 1, 3, 4, 5 });
+            combinationLock.EnterDigit(1);
+            Console.WriteLine(combinationLock.Status);
+
+            combinationLock.EnterDigit(2);
+            Console.WriteLine(combinationLock.Status);
+
+            combinationLock.EnterDigit(3);
+            Console.WriteLine(combinationLock.Status);
+
+            combinationLock.EnterDigit(4);
+            Console.WriteLine(combinationLock.Status);
+
+            combinationLock.EnterDigit(5);
+            Console.WriteLine(combinationLock.Status);
+        }
+    }
+
+    public enum EnumStatus
+    {
+        LOCKED,
+        ERROR,
+        OPEN,
+        TYPING
+    }
+
+    public class CombinationLock
+    {
+        private int[] combination;
+        private int elementAt = 0;
+
+        public CombinationLock(int[] combination)
+        {
+            this.combination = combination;
+        }
+
+        // you need to be changing this on user input
+        public EnumStatus ActualStatus = EnumStatus.LOCKED;
+        public string Status = nameof(EnumStatus.LOCKED);
+
+        public void EnterDigit(int digit)
+        {
+            switch (ActualStatus)
+            {
+                case (EnumStatus.LOCKED):
+                    if (CheckDighit(digit))
+                    {
+                        Status = digit.ToString();
+                        ActualStatus = EnumStatus.TYPING;
+                        elementAt = 1;
+                    }
+                    else
+                    {
+                        Status = nameof(EnumStatus.ERROR);
+                        ActualStatus = EnumStatus.ERROR;
+                    }
+                    break;
+                case EnumStatus.TYPING:
+                    if (CheckDighit(digit))
+                    {
+                        Status += digit.ToString();
+                        elementAt++;
+
+                        if (elementAt == combination.Length)
+                        {
+                            ActualStatus = EnumStatus.OPEN;
+                            Status = nameof(EnumStatus.OPEN);
+                        }
+                    }
+                    else
+                    {
+                        ActualStatus = EnumStatus.ERROR;
+                        Status = nameof(EnumStatus.ERROR);
+                    }
+                    break;
+                case EnumStatus.ERROR:
+                    break;
+                case EnumStatus.OPEN:
+                    break;
+            }
+        }
+
+        private bool CheckDighit(int digit)
+        {
+            return digit == combination[elementAt];
+        }
+    }
+}
