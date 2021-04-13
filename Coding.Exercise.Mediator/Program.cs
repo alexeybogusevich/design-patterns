4384ae8744f57b5989e1a1b4f7c179af4a03b26a
Oleksii Bohusevych <olboh@smart-it.com>
2021-04-13 10:05:57 +0300

    memento + mediator

---
+++ Coding.Exercise.Mediator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coding.Exercise.Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mediator = new Mediator();
+            var p1 = new Participant(mediator);
+            var p2 = new Participant(mediator);
+            p1.Say(1);
+            Console.WriteLine($"P1: {p1}");
+            Console.WriteLine($"P2: {p2}");
+            p2.Say(2);
+            Console.WriteLine($"P1: {p1}");
+            Console.WriteLine($"P2: {p2}");
+        }
+    }
+        
+    public class Participant
+    {
+        private readonly Mediator mediator;
+        public Guid Id { get; private set; }
+        public int Value { get; set; }
+
+        public Participant(Mediator mediator)
+        {
+            Id = Guid.NewGuid();
+            this.mediator = mediator;
+            mediator.Participants.Add(this);
+        }
+
+        public void Say(int n)
+        {
+            mediator.Broadcast(Id, n);
+        }
+
+        public override string ToString()
+        {
+            return $"Value: {Value}";
+        }
+    }
+
+    public class Mediator
+    {
+        public List<Participant> Participants { get; private set; }
+
+        public Mediator()
+        {
+            Participants = new List<Participant>();
+        }
+
+        public void Broadcast(Guid sourceId, int value)
+        {
+            foreach (var p in Participants.Where(x => x.Id != sourceId))
+            {
+                p.Value = value;
+            }
+        }
+    }
+}
---
+++ Coding.Exercise.Memento/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Coding.Exercise.Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var token1 = new Token(1);
+            var token2 = new Token(2);
+            var token3 = new Token(3);
+            var tokenMachine = new TokenMachine();
+            var m1 = tokenMachine.AddToken(token1);
+            var m2 = tokenMachine.AddToken(token2);
+            var m3 = tokenMachine.AddToken(token3);
+
+            Console.WriteLine(tokenMachine);
+
+            tokenMachine.Revert(m2);
+            Console.WriteLine(tokenMachine);
+        }
+    }
+
+    public class Token
+    {
+        public int Value = 0;
+
+        public Token() { }
+
+        public Token(int value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public class Memento
+    {
+        public List<int> Values { get; private set; }
+
+        public Memento() { }
+
+        public Memento(List<Token> tokens)
+        {
+            Values = tokens.Select(t => t.Value).ToList();
+        }
+    }
+
+    public class TokenMachine
+    {
+        public List<Token> Tokens = new List<Token>();
+
+        public Memento AddToken(int value)
+        {
+            var token = new Token(value);
+            Tokens.Add(token);
+            return new Memento(Tokens);
+        }
+
+        public Memento AddToken(Token token)
+        {
+            Tokens.Add(token);
+            return new Memento(Tokens);
+        }
+
+        public void Revert(Memento m)
+        {
+            Tokens = new List<Token>(m.Values.Select(v => new Token(v)).ToList());
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", Tokens.Select(t => t.Value));
+        }
+    }
+}
